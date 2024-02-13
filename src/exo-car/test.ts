51cbd899dc46146f30132caf794ca177cd44eb2d
ameliapham <ptt.huong.8895@gmail.com>
2024-02-13 05:22:23 +0100

    Exo car : Update test.ts

--- src/exo-car/test.ts
+++ src/exo-car/test.ts
@@ -1,4 +1,6 @@
-import { createCitroenC3Car } from "./createCitroenC3Car"
+import { createCitroenC3Car } from "./createCitroenC3Car";
+import { calculateAdjustedAmount } from "./calculateAdjustedAmount"
+import {bankAccount} from "./bankAccount"
 
 const myCar = createCitroenC3Car({
     "color": "pink"
@@ -7,16 +9,14 @@ const myCar = createCitroenC3Car({
 console.log(`My car ${myCar.brand} ${myCar.color} is filled at ${myCar.tankFillPercentage.toFixed(2)}%`)
 
 const { change } = myCar.putGaz({
-    "amountEur": 60
+    "amountEur": calculateAdjustedAmount({
+        "desiredAmount": 40
+    })
 })
 
 console.log(`My car's tank is filled at ${myCar.tankFillPercentage.toFixed(2)}%, I got ${change.toFixed(2)}€ in change`);
 
-/*myCar.drive({
-    "distanceKm": 160
-})
-
-console.log(`My car is filled at ${myCar.tankFillPercentage.toFixed(2)}%`)*/
+console.log(`My current bank balance is ${bankAccount.currentBankBalance}€`)
 
 try {
 
@@ -29,9 +29,13 @@ try {
     // console.log("attention on a eu une error", (error as any).message);
 
     myCar.putGaz({
-        "amountEur": 10
+        "amountEur": calculateAdjustedAmount({
+            "desiredAmount": 40
+        })
     });
 
     console.log(`My car's tank is filled at ${myCar.tankFillPercentage.toFixed(2)}%`);
 
-}
+    console.log(`My current bank balance is ${bankAccount.currentBankBalance}€`)
+};
+
