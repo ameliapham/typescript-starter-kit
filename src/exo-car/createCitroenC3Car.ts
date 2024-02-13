1c44d2cdfea10c64527b5e943b0205f571d3f64c
ameliapham <ptt.huong.8895@gmail.com>
2024-02-13 02:21:38 +0100

    Exo car : Update function putGaz inside function createCitroenC3Car (with example comments)

--- src/exo-car/createCitroenC3Car.ts
+++ src/exo-car/createCitroenC3Car.ts
@@ -9,20 +9,57 @@ export function createCitroenC3Car(
         color: string;
     }
 ): Car {
+
     const { color } = params;
+
     const car: Car = {
         color,
         "brand": "Citroen",
-        "tankFillPercentage": 0,
+        "tankFillPercentage": 0, // %
         "putGaz": ({ amountEur }) => {
+
             console.log(`We put ${amountEur}€ in Fuel`);
+
+            // Question : with amountEur €, what percentage of tanks will be full?
+
+            // dieselPrice : 1.4€/l
+            // amountEur : 50€
+            // tankFillPercentage : 0%
+            // tankCapacity : 60 litre
+
+            const volumeAddedLiter = amountEur / dieselPricePerLiter; // 50/1.4 = 35.7 liter
+
+            const volumePresentLiter = tankCapacity * car.tankFillPercentage / 100; // 60 * 0 / 100 = 0 liter
+
+            const newVolumeLiter = volumePresentLiter + volumeAddedLiter; // 35.7 liter 
+
+            car.tankFillPercentage = newVolumeLiter * 100 / tankCapacity; // 35.7 * 100 / 60 = 59.5%
+
+            // Case surplus
+            const surplusLiter = newVolumeLiter - tankCapacity; //liter
+
+            we_put_too_much_case: {
+                if (surplusLiter <= 0) {
+                    break we_put_too_much_case;
+                }
+
+                car.tankFillPercentage = 100; //%
+
+                const change = dieselPricePerLiter * surplusLiter; // eur
+
+                return { change };
+
+            }
 
             return { "change": 0 }
         },
         "drive": ({ distanceKm }) => {
+
             console.log(`Driving for ${distanceKm}km`)
+
         }
     };
+
     return car;
 }
 
