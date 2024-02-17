dd735c6829cf200cca4b59d4be494a1b30b5ba4a
ameliapham <ptt.huong.8895@gmail.com>
2024-02-17 22:21:31 +0100

    Exo date : Other way to declare new Date()

--- src/exo-date/date.ts
+++ src/exo-date/date.ts
@@ -1,5 +1,9 @@
 function greet(person: string, date: Date) {
     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-  }
-   
-  greet("Huong", new Date());
+}
+
+
+// greet("Huong", new Date());
+
+const myDate = new Date()
+greet("Huong", myDate);
