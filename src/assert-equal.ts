a2fbb9ee479522049f74180888337fe181ce17aa
ameliapham <ptt.huong.8895@gmail.com>
2024-02-19 19:45:31 +0100

    Using assert Equals to check equal of an element and type string

--- src/assert-equal.ts
+++ src/assert-equal.ts
@@ -26,3 +26,14 @@ type X = typeof x;
 assert<Equals<typeof x, X>>
 
 
+// Using assert Equals to check equal of an element and type string
+
+function getArrayElement<T>(arr: T[], index: number): T {
+    return arr[index];
+}
+
+const arr = ["a", "b", "c"];
+
+const element =getArrayElement<string>(arr, 2)
+
+assert<Equals<typeof element, string>>
